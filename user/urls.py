fe3642c5f8c96a8ceb461ac27000b867846cdde9
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2024-11-15 15:16:43 -0300

    página de login e cadastro finalizadas e criando base e index

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from user.views import Login
+from user.views import *
 
 urlpatterns = [
     path ('', Login, name="Login"),
+    path ('cadastro', Cadastro,  name="cadastro"),
+    path ('index', Index, name='index')
 ]
--- user/views.py
+++ user/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def Login(request):
     return render(request, "user/login.html")
+
+def Cadastro(request):
+    return render(request, "user/cadastro.html")
+
+def Index(request):
+    return render(request, "user/index.html")
