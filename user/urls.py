bb396ff385b9097d0e848dcf0ac52074f870e5e8
Rangel <rangelanrn@hotmail.com>
2025-01-13 20:58:26 -0300

    Criador login e cadastro dos criadores

--- user/urls.py
+++ user/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path ('cadastro', Cadastro,  name="cadastro"),
     path ('perfil', Perfil, name="perfil"),
     path ('configuracoes', Configuracoes, name="configuracoes"),
+    path ('login_criador', Login_Criador, name="login_criador"),
+    path ('cadastro_criador', Cadastro_Criador, name="cadastro_criador"),
 ]
